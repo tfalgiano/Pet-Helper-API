48b460614032b098052cf92166e8840f7c1b43f4
Tyler Falgiano <tfalgiano@gmail.com>
2024-03-29 15:47:07 -0400

    inital commit

---
+++ src/main/java/org/opensource/pethelper/pethelperapi/PetHelperApiApplication.java
@@ -0,0 +1,13 @@
+package org.opensource.pethelper.pethelperapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetHelperApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PetHelperApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/opensource/pethelper/pethelperapi/PetHelperApiApplicationTests.java
@@ -0,0 +1,13 @@
+package org.opensource.pethelper.pethelperapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetHelperApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
